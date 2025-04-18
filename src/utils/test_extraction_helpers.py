41786fbd0d9b22fc20a8dd7d3aabfb510b48c733
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-04-18 21:00:42 +0100

    test coverage for image extractor

--- src/utils/test_extraction_helpers.py
+++ src/utils/test_extraction_helpers.py
@@ -1,4 +1,4 @@
-from src.utils.extraction_helpers import extract_markdown_links
+from src.utils.extraction_helpers import extract_markdown_links, extract_markdown_images
 
 import unittest
 
@@ -22,3 +22,25 @@ class TestExtractionHelpers(unittest.TestCase):
         text = "A [real link](l) and an ![image link](i)."
         expected = [("real link", "l")]
         self.assertEqual(extract_markdown_links(text), expected)
+
+    def test_extract_images_no_images(self):
+        text = "This text has no images."
+        self.assertEqual(extract_markdown_images(text), [])
+
+    def test_extract_images_one_image(self):
+        text = "Look at this ![alt text](image.jpg)."
+        expected = [("alt text", "image.jpg")]
+        self.assertEqual(extract_markdown_images(text), expected)
+
+    def test_extract_images_multiple_images(self):
+        text = "See ![img1](url1.png) and ![img2](url2.gif)."
+        expected = [("img1", "url1.png"), ("img2", "url2.gif")]
+        self.assertEqual(extract_markdown_images(text), expected)
+
+    def test_extract_images_ignore_links(self):
+        text = "An ![image](i) and a [link](l)."
+        expected = [("image", "i")]
+        self.assertEqual(extract_markdown_images(text), expected)
+
+if __name__ == "__main__":
+    unittest.main()
