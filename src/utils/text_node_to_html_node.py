c073714c4a5801f5db75348dfafe21ade45d8430
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-04-18 20:15:48 +0100

    add fallback case for text_node -> html_node

--- src/utils/text_node_to_html_node.py
+++ src/utils/text_node_to_html_node.py
@@ -15,4 +15,6 @@ def text_node_to_html_node(text_node):
             return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text })
         case TextType.LINK:
             return LeafNode("a", text_node.text, { "href": text_node.url })
+        case _:
+            raise ValueError("Not a valid Text Node")
 
