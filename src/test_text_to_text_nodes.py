e05127c62618840e73142e0272973769c0465937
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-04-20 15:33:41 +0100

    test for text to text nodes method
    Worked first try! V satisfying

---
+++ src/test_text_to_text_nodes.py
@@ -0,0 +1,23 @@
+import unittest
+from src.text_to_text_nodes import text_to_text_nodes
+from src.text_node import TextNode, TextType
+
+class TestTextToTexNode(unittest.TestCase):
+    def test_baseline(self):
+        input = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+        expected_output = [
+            TextNode("This is ", TextType.TEXT),
+            TextNode("text", TextType.BOLD),
+            TextNode(" with an ", TextType.TEXT),
+            TextNode("italic", TextType.ITALIC),
+            TextNode(" word and a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" and an ", TextType.TEXT),
+            TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" and a ", TextType.TEXT),
+            TextNode("link", TextType.LINK, "https://boot.dev"),
+        ]
+        self.assertEqual(text_to_text_nodes(input), expected_output)
+
+if __name__ == "__main__":
+    unittest.main()
