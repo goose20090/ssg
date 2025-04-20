63a376cc95609aab0bb61f67d81f6daf0e0c5b3a
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-04-20 13:16:42 +0100

    tests for link splitting

---
+++ src/utils/test_split_link_and_image_nodes.py
@@ -0,0 +1,70 @@
+import unittest
+
+from src.text_node import TextNode, TextType
+from src.utils.split_link_and_image_nodes import split_nodes_link, split_nodes_image
+
+class TestSplitImageAndLinkNodes(unittest.TestCase):
+    def test_split_links_basic(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+            ], new_nodes
+        )
+
+    def test_link_at_start(self):
+        node = TextNode(
+            "[to boot dev](https://www.boot.dev) some more text",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" some more text", TextType.TEXT),
+            ], new_nodes
+        )
+
+    def test_link_at_end(self):
+        node = TextNode(
+            "some text at the beginning here [to boot dev](https://www.boot.dev)",
+            TextType.TEXT,
+        )
+        new_nodes = split_nodes_link([node])
+        self.assertListEqual(
+            [
+                TextNode("some text at the beginning here ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            ], new_nodes
+        )
+
+    def multiple_nodes(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT,
+        )
+        bold_node = TextNode("bold node", TextType.BOLD)
+        code_node = TextNode("code node", TextType.CODE)
+        new_nodes = split_nodes_link([code_node, node, bold_node])
+        self.assertListEqual(
+            [
+                code_node,
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode(
+                    "to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"
+                ),
+                bold_node
+            ], new_nodes
+        )
+
