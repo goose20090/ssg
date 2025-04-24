a16e94dd8c0e40848fb5440debaeabd09b510682
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2025-04-24 22:25:57 +0100

    generate pages recursively from content to public

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,5 +1,6 @@
 import os
 from block_markdown import extract_header_from_markdown, markdown_to_html_nodes
+from pathlib import Path
 
 
 def generate_page(from_path, template_path, dest_path):
@@ -18,5 +19,42 @@ def generate_page(from_path, template_path, dest_path):
     template_str = template_str.replace("{{ Title }}", title)
     template_str = template_str.replace("{{ Content }}", content)
 
+
     with open(dest_path, "w") as dest_file:
             dest_file.write(template_str)
+
+# Crawl every entry in the content directory
+# For each markdown file found, generate a new .html file using the same template.html. 
+# The generated pages should be written to the public directory in the same directory structure.
+
+
+# generate_page("./content/index.md", "./template.html", "./public/index.html")
+
+
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    if not os.path.exists(dest_dir_path):
+        os.mkdir(dest_dir_path)
+
+
+    contents = os.listdir(dir_path_content)
+
+    md_files = []
+
+    dirs = []
+
+    for file in contents:
+        path = os.path.join(dir_path_content, file)
+        if os.path.isfile(path):
+            md_files.append(file)
+        if os.path.isdir(path):
+            dirs.append(file)
+
+    for file in md_files:
+        dest_path = os.path.join(dest_dir_path, file)
+        dest_path = Path(dest_path).with_suffix(".html")
+        generate_page(os.path.join(dir_path_content, file), template_path, str(dest_path))
+
+
+    for dir in dirs:
+        generate_pages_recursive(os.path.join(dir_path_content, dir), template_path, os.path.join(dest_dir_path, dir))
+
--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-from generate_page import generate_page
+from generate_page import generate_page, generate_pages_recursive
 from text_node import TextNode, TextType
 from copy_static_to_public import copy_static_to_public
 
 def main():
 
     copy_static_to_public()
-    generate_page("./content/index.md", "./template.html", "./public/index.html")
+    generate_pages_recursive("./content", "./template.html", "./public")
 
 
 main()
